79782057063d3baae31781feca8e6328ff48673f
Irvin <irtan757@gmail.com>
2026-08-21 01:12:50 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Sandrone.java
@@ -0,0 +1,21 @@
+/**
+ * Starts the Sandrone chatbot application.
+ */
+public class Sandrone {
+    public static void main(String[] args) {
+        String banner = " SSSS    A   N   N DDDD  RRRR   OOO  N   N EEEEE\n"
+            + "S       A A  NN  N D   D R   R O   O NN  N E    \n"
+            + " SSS   AAAAA N N N D   D RRRR  O   O N N N EEEE \n"
+            + "    S  A   A N  NN D   D R R   O   O N  NN E    \n"
+            + "SSSS   A   A N   N DDDD  R  RR  OOO  N   N EEEEE\n";
+
+        System.out.println("____________________________________________________________");
+        System.out.println(banner);
+        System.out.println("Tch... Hello. I'm Sandrone. ...Don't make me say it again.");
+        System.out.println("What do you want?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye.");
+        System.out.println();
+        System.out.println("____________________________________________________________");
+    }
+}
