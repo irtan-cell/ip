ff89c54e38d700ed698e7a1b4d17e8a68d059e21
Irvin <irtan757@gmail.com>
2026-08-23 16:50:57 +0800

    Level 5. Handle Errors
    added method for handling creation of task
    to abstract out logic of that
    
    added SandroneException class to be treated
    differently from normal exceptions in try catch
    
    added exception handling for empty description
    and invalid commands

--- src/main/java/Sandrone.java
+++ src/main/java/Sandrone.java
@@ -29,72 +29,65 @@ public class Sandrone {
         int numberOfTasks = 0;
 
         while (true) {
-            String command = scanner.nextLine();
+            try {
+                String command = scanner.nextLine();
 
-            if (command.equals("bye")) {
-                break;
-            } else if (command.equals("list")) {
-                printLine(false);
-                System.out.println(" Here are the tasks in your list:");
-                for (int taskNumber = 0; taskNumber < numberOfTasks; taskNumber++) {
-                    System.out.println(" " + (taskNumber + 1) + "." + tasks[taskNumber]);
-                }
-                printLine(true);
-            } else if (command.startsWith("mark ")) {
-                int taskNumber = Integer.parseInt(command.substring(5));
-                if (taskNumber <= 0 || taskNumber > numberOfTasks) {
-                    printMessage("Invalid task index");
-                    continue;
-                }
-                int taskIndex = taskNumber - 1;
-                tasks[taskIndex].markAsDone();
-                printLine(false);
-                System.out.println(" Nice! I've marked this task as done:");
-                System.out.println("   [" + tasks[taskIndex].getStatusIcon() + "] "
-                    + tasks[taskIndex].getDescription());
-                printLine(true);
-            } else if (command.startsWith("unmark ")) {
-                int taskNumber = Integer.parseInt(command.substring(7));
-                if (taskNumber <= 0 || taskNumber > numberOfTasks) {
-                    printMessage("Invalid task index");
-                    continue;
-                }
-                int taskIndex = taskNumber - 1;
-                tasks[taskIndex].markAsNotDone();
-                printLine(false);
-                System.out.println(" OK, I've marked this task as not done yet:");
-                System.out.println("   [" + tasks[taskIndex].getStatusIcon() + "] "
-                    + tasks[taskIndex].getDescription());
-                printLine(true);
-            } else {
-                if (command.startsWith("todo ")) {
-                    String description = command.substring(5);
-                    tasks[numberOfTasks] = new Todo(description);
-                } else if (command.startsWith("deadline ")) {
-                    String rest = command.substring(9);
-                    String[] parts = rest.split(" /by ");
-                    String description = parts[0];
-                    String by = parts.length > 1 ? parts[1] : "";
-                    tasks[numberOfTasks] = new Deadline(description, by);
-                } else if (command.startsWith("event ")) {
-                    String rest = command.substring(6);
-                    String[] fromParts = rest.split(" /from ");
-                    String description = fromParts[0];
+                if (command.equals("bye")) {
+                    break;
 
-                    String fromAndTo = fromParts.length > 1 ? fromParts[1] : "";
-                    String[] toParts = fromAndTo.split(" /to ");
-                    String from = toParts[0];
-                    String to = toParts.length > 1 ? toParts[1] : "";
-                    tasks[numberOfTasks] = new Event(description, from, to);
+                } else if (command.equals("list")) {
+                    printLine(false);
+                    System.out.println(" Here are the tasks in your list:");
+                    for (int taskNumber = 0; taskNumber < numberOfTasks; taskNumber++) {
+                        System.out.println(" " + (taskNumber + 1) + "." + tasks[taskNumber]);
+                    }
+                    printLine(true);
+
+                } else if (command.startsWith("mark ")) {
+                    int taskNumber = Integer.parseInt(command.substring(5));
+                    if (taskNumber <= 0 || taskNumber > numberOfTasks) {
+                        printMessage("Invalid task index");
+                        continue;
+                    }
+                    int taskIndex = taskNumber - 1;
+                    tasks[taskIndex].markAsDone();
+                    printLine(false);
+                    System.out.println(" Nice! I've marked this task as done:");
+                    System.out.println("   [" + tasks[taskIndex].getStatusIcon() + "] "
+                        + tasks[taskIndex].getDescription());
+                    printLine(true);
+
+                } else if (command.startsWith("unmark ")) {
+                    int taskNumber = Integer.parseInt(command.substring(7));
+                    if (taskNumber <= 0 || taskNumber > numberOfTasks) {
+                        printMessage("Invalid task index");
+                        continue;
+                    }
+                    int taskIndex = taskNumber - 1;
+                    tasks[taskIndex].markAsNotDone();
+                    printLine(false);
+                    System.out.println(" OK, I've marked this task as not done yet:");
+                    System.out.println("   [" + tasks[taskIndex].getStatusIcon() + "] "
+                        + tasks[taskIndex].getDescription());
+                    printLine(true);
+
+                } else if (command.startsWith("todo ") || command.startsWith("deadline ")
+                    || command.startsWith("event ")) {
+                    Task task = createTask(command);
+                    if (task == null) {
+                        continue;
+                    }
+                    tasks[numberOfTasks] = task;
+                    numberOfTasks++;
+                    printLine(false);
+                    System.out.println(" added: " + command);
+                    System.out.println("You now have " + numberOfTasks + " tasks in the list");
+                    printLine(true);
                 } else {
-                    printMessage("Invalid command, use case: todo/deadline/event item");
-                    continue;
+                    throw new SandroneException("Invalid messaage");
                 }
-                numberOfTasks++;
-                printLine(false);
-                System.out.println(" added: " + command);
-                System.out.println("You now have " + numberOfTasks + " tasks in the list");
-                printLine(true);
+            } catch (SandroneException e) {
+                printMessage("Oops! " + e.getMessage());
             }
         }
         scanner.close();
@@ -113,5 +106,41 @@ public class Sandrone {
         printLine(false);
         System.out.println(message);
         printLine(true);
+    }
+
+    public static Task createTask(String command) throws SandroneException {
+        if (command.startsWith("todo ")) {
+            String description = command.substring(5);
+            if (description.trim().isEmpty()) {
+                throw new SandroneException("Description cannot be empty");
+            }
+            return new Todo(description);
+        } else if (command.startsWith("deadline ")) {
+            String rest = command.substring(8);
+            System.out.println("rest: " + rest);
+            String[] parts = rest.split(" /by ");
+            String description = parts[0];
+            if (description.trim().isEmpty()) {
+                throw new SandroneException("Description cannot be empty");
+            }
+
+            String by = parts.length > 1 ? parts[1] : "";
+            return new Deadline(description, by);
+        } else if (command.startsWith("event ")) {
+            String rest = command.substring(5);
+            String[] fromParts = rest.split(" /from ");
+            String description = fromParts[0];
+            if (description.trim().isEmpty()) {
+                throw new SandroneException("Description cannot be empty");
+            }
+
+            String fromAndTo = fromParts.length > 1 ? fromParts[1] : "";
+            String[] toParts = fromAndTo.split(" /to ");
+            String from = toParts[0];
+            String to = toParts.length > 1 ? toParts[1] : "";
+            return new Event(description, from, to);
+        } else {
+            return null;
+        }
     }
 }
---
+++ src/main/java/SandroneException.java
@@ -0,0 +1,5 @@
+public class SandroneException extends Exception {
+    public SandroneException(String message) {
+        super(message);
+    }
+}
